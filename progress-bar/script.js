d6b91435ccd84606bf58f34a94102fcaf58c0168
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-10-08 14:19:30 -0300

    setting action to next button

--- progress-bar/script.js
+++ progress-bar/script.js
@@ -2,3 +2,15 @@ const progressBar = document.getElementById('progress')
 const progressBarPrev = document.getElementById('prev')
 const progressBarNext = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+progressBarNext.addEventListener('click', () => {
+    currentActive++
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    console.log(currentActive)
+})

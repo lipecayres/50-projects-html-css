a21650d0e7caf37fc90740976b3cb35ed2e5249e
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-10-08 14:19:09 -0300

    Getting elementos from html

---
+++ progress-bar/script.js
@@ -0,0 +1,4 @@
+const progressBar = document.getElementById('progress')
+const progressBarPrev = document.getElementById('prev')
+const progressBarNext = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')

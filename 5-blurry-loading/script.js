7d96f5e0a02ae881e14bb612eaa0b4bf273eb8e2
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-10-15 16:12:18 -0300

    bg blur ok

--- 5-blurry-loading/script.js
+++ 5-blurry-loading/script.js
@@ -15,6 +15,7 @@ function blurring() {
 
     loadText.innerHTML = `${load}%`
     loadText.style.opacity = scale(load, 0, 100 , 1,0)
+    bg.style.filter = `blur(${scale(load,0, 100, 30, 0)}px)`
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {

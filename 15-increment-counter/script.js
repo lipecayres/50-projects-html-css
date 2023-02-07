163f2e0b4b971ae998a748c987131bca97e3f864
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-02-06 20:36:05 -0500

    condition to set counter on screen at same speed

--- 15-increment-counter/script.js
+++ 15-increment-counter/script.js
@@ -8,6 +8,13 @@ counters.forEach(counter => {
         const c = +counter.innerText
 
         const increment = target / 200
+
+        if (c<target){
+            counter.innerText = `${Math.ceil(c+increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
     }
 
     updateCounter()

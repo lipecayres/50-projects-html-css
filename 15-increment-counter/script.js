54a4e3fc831faf61623bff063c18c857521fd03c
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-02-06 20:29:39 -0500

    variables to counter

--- 15-increment-counter/script.js
+++ 15-increment-counter/script.js
@@ -2,4 +2,13 @@ const counters = document.querySelectorAll('.counter')
 
 counters.forEach(counter => {
     counter.innerHTML = '0'
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target / 200
+    }
+
+    updateCounter()
 })

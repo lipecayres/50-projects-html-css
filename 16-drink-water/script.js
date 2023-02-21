412177a94a87c8945a0baccdbffa5218b39f68ef
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-02-20 20:11:07 -0500

    selecting small cups length to fill big cup and hiding if non clicked

--- 16-drink-water/script.js
+++ 16-drink-water/script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigCup()
+
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
 })
@@ -21,4 +23,16 @@ function highlightCups(idx){
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
+}
+
+function updateBigCup(){
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const allCups = smallCups.length
+
+    if (fullCups == 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    }
 }

324ad9fb10b8872c31a7b10df040bff59f333d37
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-02-25 22:47:46 -0500

    getting links to api

---
+++ 17-movie-app/script.js
@@ -0,0 +1,6 @@
+    // URL's to use API
+
+const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8e837886524772327b33dd2f1553c14d&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'http://api.themoviedb.org/3/search/movie?api_key=8e837886524772327b33dd2f1553c14d&querry"'
+

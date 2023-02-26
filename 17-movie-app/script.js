309256bd6e9e2fa620a08b568c365c483ae36ef5
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-02-25 22:48:10 -0500

    search event on page

--- 17-movie-app/script.js
+++ 17-movie-app/script.js
@@ -4,6 +4,22 @@ const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'http://api.themoviedb.org/3/search/movie?api_key=8e837886524772327b33dd2f1553c14d&querry"'
 
+    // HTML link's
+
+    const form = document.getElementById('form')
+    const search = document.getElementById('search')
+    
+        // Search event
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+    
+        const searchTerm = search.value 
+    
+        if(searchTerm && searchTerm !== ''){
+            getMovies()
+        }
+    })
+    
     // Get Initial Movies
     getMovies(API_URL)
     

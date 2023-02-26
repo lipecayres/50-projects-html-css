419ce8ac66fed839829c54d85b4b56301e690a63
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-02-25 22:47:59 -0500

    function to get movies

--- 17-movie-app/script.js
+++ 17-movie-app/script.js
@@ -4,3 +4,12 @@ const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'http://api.themoviedb.org/3/search/movie?api_key=8e837886524772327b33dd2f1553c14d&querry"'
 
+    // Get Initial Movies
+    getMovies(API_URL)
+    
+    async function getMovies(url) {
+        const res = await fetch(url)
+        const data = await res.json()
+    
+        console.log(data.results)
+    }

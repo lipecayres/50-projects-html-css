0421b8d96c09d70db0f55dfd3b14283c48431a9a
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-05-02 23:00:19 -0400

    getting elements

---
+++ 18-backgroung-slider/script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('slider-container')
+const slide = document.querySelectorAll('slide')
+
+const btnLeft = document.getElementById('left')
+const btnRight = document.getElementById('right')
+

64443c44fd87b80b227bb37bb576e1bb11420639
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-06-20 11:46:56 -0400

    add clicks left/right to buttons

--- 18-backgroung-slider/script.js
+++ 18-backgroung-slider/script.js
@@ -2,15 +2,37 @@ const body = document.body
 const container = document.querySelector('.slider-container')
 const slides = document.querySelectorAll('.slide')
 
-const btnLeft = document.getElementById('.left')
-const btnRight = document.getElementById('.right')
+const btnLeft = document.getElementById('left')
+const btnRight = document.getElementById('right')
 
 // slides position
-let activeSlide = 0
+let activeSlide =0
+
+btnRight.addEventListener('click', () => {
+    activeSlide++
+
+    if(activeSlide >slides.length -1) {
+        activeSlide = 0
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+btnLeft.addEventListener('click', () => {
+    activeSlide--
+
+    if(activeSlide <0) {
+        activeSlide = slides.length -1
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
 
 setBgToBody()
 
-setActiveSlide()
+
 function setBgToBody () {
     body.style.backgroundImage = slides[activeSlide].style.backgroundImage
 }
@@ -20,3 +42,11 @@ function setActiveSlide() {
 
     slides[activeSlide].classList.add('active')
 }
+
+function leftClick () {
+    if (activeSlide < 0) {
+        activeSlide = 4
+    } 
+    activeSlide--
+}
+

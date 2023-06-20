a1743a03249febf145affc4a22b0ecc9efb81f58
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-06-20 11:34:04 -0400

    debug/ add . to get classes

--- 18-backgroung-slider/script.js
+++ 18-backgroung-slider/script.js
@@ -1,15 +1,22 @@
 const body = document.body
-const container = document.querySelector('slider-container')
-const slides = document.querySelectorAll('slide')
+const container = document.querySelector('.slider-container')
+const slides = document.querySelectorAll('.slide')
 
-const btnLeft = document.getElementById('left')
-const btnRight = document.getElementById('right')
+const btnLeft = document.getElementById('.left')
+const btnRight = document.getElementById('.right')
 
 // slides position
 let activeSlide = 0
 
-setBgToBody();
+setBgToBody()
 
+setActiveSlide()
 function setBgToBody () {
     body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach((slide) => slide.classList.remove('active'))
+
+    slides[activeSlide].classList.add('active')
 }

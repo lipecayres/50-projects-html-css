a7421762bfcbf6475192aeabe7d3aa7c191a2c80
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-06-20 11:15:23 -0400

    function to set background to body

--- 18-backgroung-slider/script.js
+++ 18-backgroung-slider/script.js
@@ -1,6 +1,15 @@
+const body = document.body
 const container = document.querySelector('slider-container')
-const slide = document.querySelectorAll('slide')
+const slides = document.querySelectorAll('slide')
 
 const btnLeft = document.getElementById('left')
 const btnRight = document.getElementById('right')
 
+// slides position
+let activeSlide = 0
+
+setBgToBody();
+
+function setBgToBody () {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}

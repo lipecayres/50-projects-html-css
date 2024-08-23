7ac3959a0ebdda0d470ed43d0d39ca9c337e3c1c
Felipe Cayres <felipescayres@gmail.com>
2024-08-22 23:28:09 -0400

    feature[clock]: added time details AM PM

--- 19-theme-clock/script.js
+++ 19-theme-clock/script.js
@@ -5,6 +5,7 @@ const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('date')
 const toggle = document.querySelector('.toggle')
 
+
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', "Thursday", 'Friday', 'Saturday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', "Mai", 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
@@ -29,10 +30,13 @@ function setTime() {
     const hoursforClock = hours %12;
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM'
 
     hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursforClock, 0, 11, 0, 360)}deg)`
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hoursforClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
 }
 
 const scale = (num,in_min, in_max, out_min, out_max) => {

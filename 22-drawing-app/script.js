47bcca2221ec496308e013e6122a675fc886c287
Felipe Cayres <felipescayres@gmail.com>
2024-10-16 14:05:18 -0400

    drawline finction ok

--- 22-drawing-app/script.js
+++ 22-drawing-app/script.js
@@ -11,4 +11,14 @@ function drawCircle(x, y) {
   ctx.fill();
 }
 
+function drawLine (x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size
+    ctx.stroke()
+}
+
 drawCircle(100,200);
+drawLine(300,300,500,500)

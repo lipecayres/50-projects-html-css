29eb6a8f8a9be0da380a81b2ff7fc01cd4c1e5ac
Felipe Cayres <felipescayres@gmail.com>
2024-10-16 15:02:54 -0400

    clear function done

--- 22-drawing-app/script.js
+++ 22-drawing-app/script.js
@@ -1,8 +1,55 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+
+const clear = document.getElementById('clear');
+
+let isPressed = false;
+
 let size = 20;
-let color = 'black';
+let color = "black";
+
+let x, y;
+let x1, y1, x2, y2;
+
+
+clear.addEventListener ('click', () => {
+    ctx.clearRect(0, 0, 804, 804)
+})
+
+
+
+// Drawing movements
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
+
+  console.log(isPressed, x, y);
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+
+  console.log(isPressed, x, y);
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2,y2)
+    drawLine(x,y,x2,y2)
+
+    x= x2
+    y = y2
+  }
+});
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -11,14 +58,14 @@ function drawCircle(x, y) {
   ctx.fill();
 }
 
-function drawLine (x1, y1, x2, y2) {
-    ctx.beginPath();
-    ctx.moveTo(x1,y1);
-    ctx.lineTo(x2,y2);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = size
-    ctx.stroke()
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size *2;
+  ctx.stroke();
 }
 
-drawCircle(100,200);
-drawLine(300,300,500,500)
+drawCircle(100, 200);
+drawLine(300, 300, 500, 500);

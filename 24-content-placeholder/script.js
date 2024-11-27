26adf0064013a3ace8975062322d706100caca85
Felipe Cayres <felipescayres@gmail.com>
2024-11-27 13:45:17 -0500

    setted timeout for display content after 2 seconds

---
+++ 24-content-placeholder/script.js
@@ -0,0 +1,12 @@
+const animated_bg = document.querySelectorAll(".animated-bg");
+
+setTimeout(() => {
+    const loaders = document.querySelectorAll('.animated-bg');
+    const loadersText = document.querySelectorAll(".animated-bg-text");
+    loaders.forEach(loader => {
+        loader.classList.remove('animated-bg');
+    });
+    loadersText.forEach(loader => {
+        loader.classList.remove('animated-bg-text');
+    });
+}, 2000); // 2 seconds
